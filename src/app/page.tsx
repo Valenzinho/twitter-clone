7221729a8c2e4da69246b3788c0c45417cb9a938
Valenzinho <barberisvalentin93@gmail.com>
2024-10-31 18:45:53 -0300

    Continuando con el proyecto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
+import { AuthButton } from "@/components/auth-button";
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('posts').select('*')
+  // Pasamos cookies directamente como una función
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data: posts } = await supabase.from("posts").select("*");
 
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hola Twitter
-        <pre>
-          {
-            JSON.stringify(posts, null, 2)
-          }
-        </pre>
-      </main> 
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <AuthButton />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </main>
   );
 }
